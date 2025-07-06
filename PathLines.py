5a01e36125223e70179ebb0c188644e75fb6d1d2
Stacy X <vagnasty2015@gmail.com>
2025-07-06 22:55:55 +0700

    change

--- PathLines.py
+++ PathLines.py
@@ -13,4 +13,10 @@ class LineTo:
     def __init__(self, x, y):
         self.__x = x
         self.__y = y
-    
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y

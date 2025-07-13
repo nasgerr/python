7fdf475a7a4151fc428533e1ae30708b23954e4d
Stacy X <vagnasty2015@gmail.com>
2025-07-13 19:27:11 +0700

    add new completed task

---
+++ InputDigits.py
@@ -0,0 +1,7 @@
+class InputDigits:
+    def __init__(self, func):
+        self.func = func
+    def __call__(self, *args, **kwargs):
+        return list(map(int, self.func().split()))
+input_dg = InputDigits(input)
+res = input_dg()

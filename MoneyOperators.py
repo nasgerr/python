2bc9e235038ac570dd13fa2f0177b6f49b15cee8
Stacy X <vagnasty2015@gmail.com>
2025-07-28 21:08:40 +0700

    add new completed task

---
+++ MoneyOperators.py
@@ -0,0 +1,43 @@
+class MoneyOperators:
+    def __add__(self, other):
+        if type(other) in (int, float):
+            return self.__class__(self.money + other)
+
+        if type(self) != type(other):
+            raise TypeError('Разные типы объектов')
+
+        return self.__class__(self.money + other.money)
+
+    def __sub__(self, other):
+        if type(other) in (int, float):
+            return self.__class__(self.money - other)
+
+        if type(self) != type(other):
+            raise TypeError('Разные типы объектов')
+
+        return self.__class__(self.money - other.money)
+
+
+class Money:
+    def __init__(self, value):
+        self.money = value
+
+    @property
+    def money(self):
+        return self._money
+
+    @money.setter
+    def money(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError('сумма должна быть числом')
+        self._money = value
+
+
+class MoneyR(Money, MoneyOperators):
+    def __str__(self):
+        return f"MoneyR: {self.money}"
+
+
+class MoneyD(Money, MoneyOperators):
+    def __str__(self):
+        return f"MoneyD: {self.money}"
